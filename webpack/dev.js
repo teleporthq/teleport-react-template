ec9fb85d19625ccaf5050181836e32367a4f2d1a
JayaKrishnaNamburu <namburu1995@gmail.com>
2021-10-28 15:03:57 +0530

    Update template

--- webpack/dev.js
+++ webpack/dev.js
@@ -3,16 +3,16 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./common");
 
 module.exports = (env) => merge(commonConfig(env), {
-  mode: "development",
-  entry: [
-    "webpack-dev-server/client?http://localhost:8080",
-    "./index.tsx",
-  ],
-  devServer: {
-    hot: "only",
-    historyApiFallback: true,
-    static: join(__dirname, '../public')
-  },
-  devtool: "cheap-module-source-map",
-  plugins: [],
+    mode: "development",
+    entry: [
+        "webpack-dev-server/client?http://localhost:8080",
+        "./index.js",
+    ],
+    devServer: {
+        hot: "only",
+        historyApiFallback: true,
+        static: join(__dirname, '../public')
+    },
+    devtool: "cheap-module-source-map",
+    plugins: [],
 });
--- webpack/prod.js
+++ webpack/prod.js
@@ -6,49 +6,38 @@ const CopyPlugin  = require('copy-webpack-plugin')
 
 
 module.exports = env => merge(commonConfig(env), {
-  mode: "production",
-  entry: resolve(__dirname, '../src/index.tsx'),
-  output: {
-    filename: "js/bundle.[contenthash].min.js",
-    path: resolve(__dirname, "../build"),
-    publicPath: "/",
-    assetModuleFilename: 'images/[hash][ext][query]'
-  },
-  plugins: [
-    new TerserPlugin({ terserOptions: {keep_fnames: true, keep_classnames: true, }}),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: '../public/playground_assets',
-          to() {
-            return join(__dirname, '../build', 'playground_assets');
-          },
-        },
-      ]
-    })
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [],
-    nodeEnv: 'production',
-    sideEffects: true,
-    concatenateModules: true,
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: 10,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
-            )[1];
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
+    mode: "production",
+    entry: resolve(__dirname, '../src/index.js'),
+    output: {
+        filename: "js/bundle.[contenthash].min.js",
+        path: resolve(__dirname, "../build"),
+        publicPath: "/",
+        assetModuleFilename: 'images/[hash][ext][query]'
     },
-  }
+    plugins: [
+        new TerserPlugin({ terserOptions: {keep_fnames: true, keep_classnames: true, }}),
+        new CopyPlugin({
+        patterns: [
+            {
+                from: '../public/playground_assets',
+                to() {
+                    return join(__dirname, '../build', 'playground_assets');
+                },
+            },
+        ]
+        })
+    ],
+    optimization: {
+            minimize: true,
+            minimizer: [],
+            nodeEnv: 'production',
+            sideEffects: true,
+            concatenateModules: true,
+            runtimeChunk: 'single',
+            splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: 10,
+            minSize: 0,
+        },
+    }
 });
